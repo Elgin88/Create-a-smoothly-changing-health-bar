55dfaa1ea112838bdf9b97535b5abdce981a7945
Elgin88 <elgin-88@mail.ru>
2022-09-01 16:55:29 +0800

    1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private int _maxHealth = 100;
+
     private Animator _animator;
     private string _idleRight = "IdleRight";
+    private int _currentHealth;
 
     public void Start()
     {
         _animator = GetComponent<Animator>();
+        _currentHealth = _maxHealth;
+
         StartCoroutine(PlayAnimation());
     }
 
@@ -20,6 +25,26 @@ public class Player : MonoBehaviour
         {
             _animator.Play(_idleRight);
             yield return null;
+        }
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _currentHealth -= damage;
+
+        if (_currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ApplyHealing(int damage)
+    {
+        _currentHealth += damage;
+
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
         }
     }
 }
